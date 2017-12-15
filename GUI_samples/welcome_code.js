8689191d194d4caf19b8d5fbb6e2aa7721944a41
Dania801 <ssansnnom07@gmail.com>
2017-12-15 19:42:58 +0200

    Finished the journal page completely

--- GUI_samples/welcome_code.js
+++ GUI_samples/welcome_code.js
@@ -1,4 +1,4 @@
-$(document).ready(function(){
+$(document).ready(() => {
   $("#article").hide();
   $("#search-panel").hide();
   $("#first-article").hide();
@@ -9,41 +9,41 @@ $(document).ready(function(){
   $("#third-article").show(1500);
 
   // Add smooth scrolling
-  $(".navbar a, footer a[href='#myPage'], footer a[href='#home'], nav a, #add-btn, #footer, #article").on('click', function(event) {
+  $(".navbar a, footer a[href='#myPage'], footer a[href='#home'], nav a, #add-btn, #footer, #article").on("click", function (event) {
     // Make sure this.hash has a value before overriding default behavior
     if (this.hash !== "") {
       // Prevent default anchor click behavior
       event.preventDefault();
       // Store hash
-      var hash = this.hash;
+      let hash = this.hash;
       // Using jQuery's animate() method to add smooth page scroll
       // The optional number (900) specifies the number of milliseconds it takes to scroll to the specified area
       $('html, body').animate({
         scrollTop: $(hash).offset().top
-      }, 900, function(){
+      }, 900, () => {
         // Add hash (#) to URL when done scrolling (default click behavior)
         window.location.hash = hash;
       });
     }
   });
   // close sidebar smoothly
-  $("#sidebar-x").on("click", function(){
+  $("#sidebar-x").on("click", () => {
     $("#sidebar-x").hide(500);
     $("#mySidebar").slideUp(500);
     $("#myOverlay").hide();
   });
   // show sidebar
-  $("#sidebar-btn").on("click", function(){
+  $("#sidebar-btn").on("click", () => {
     document.getElementById("mySidebar").style.display = "block";
     $("#sidebar-x").show(500);
     document.getElementById("myOverlay").style.display = "block";
   });
   // show search panel
-  $("#search-btn").on("click",function(){
+  $("#search-btn").on("click", () => {
     $("#search-panel").slideToggle(1000);
   });
   // animate elements when scrolling
-  $(window).scroll(function() {
+  $(window).scroll(() =>  {
     $(".slideanim").each(function(){
       var pos = $(this).offset().top;
 
@@ -58,16 +58,13 @@ $(document).ready(function(){
 function showLogin(){
   $("#login").show() ;
 }
-
 function closeLogin(){
   $("#login").hide(500) ;
 }
-
 function showPic(pic){
   document.getElementById('pic_container').src = pic;
   $("#picture").show() ;
 }
-
 function showArticle(pic){
   document.getElementById('article-pic').src = pic;
   $("#article").show() ;
