e62654c4e09c7f8d554f2e987a35bb6f0084e791
Dania801 <ssansnnom07@gmail.com>
2018-01-09 21:03:14 +0200

    Moved the content of Events page to controller

--- app_server/controllers/events.js
+++ app_server/controllers/events.js
@@ -1,3 +1,70 @@
 module.exports.index = function(req , res){
-  res.render('index' , {title: 'Blogate'}) ;
+  res.render('events' , {
+    title: 'Events',
+    userInfo: {
+      firstName: 'Dania',
+      lastName: 'Refaie',
+      profilePic: '/images/Home/pictures/album2.jpg',
+      email: 'example1@gmail.com',
+      password: '123'
+    },events: [
+      {
+        title: 'Review mockups',
+        fromDay: 12,
+        fromMonth: 'Dec',
+        fromYear: '2017',
+        toDay: 15,
+        toMonth: 'Jan',
+        toYear: '2018'
+      },{
+        title: 'Review mockups',
+        fromDay: 12,
+        fromMonth: 'Dec',
+        fromYear: '2017',
+        toDay: 15,
+        toMonth: 'Jan',
+        toYear: '2018'
+      },{
+        title: 'Review mockups',
+        fromDay: 12,
+        fromMonth: 'Dec',
+        fromYear: '2017',
+        toDay: 15,
+        toMonth: 'Jan',
+        toYear: '2018'
+      },{
+        title: 'Review mockups',
+        fromDay: 12,
+        fromMonth: 'Dec',
+        fromYear: '2017',
+        toDay: 15,
+        toMonth: 'Jan',
+        toYear: '2018'
+      },{
+        title: 'Review mockups',
+        fromDay: 12,
+        fromMonth: 'Dec',
+        fromYear: '2017',
+        toDay: 15,
+        toMonth: 'Jan',
+        toYear: '2018'
+      },{
+        title: 'Review mockups',
+        fromDay: 12,
+        fromMonth: 'Dec',
+        fromYear: '2017',
+        toDay: 15,
+        toMonth: 'Jan',
+        toYear: '2018'
+      },{
+        title: 'Review mockups',
+        fromDay: 12,
+        fromMonth: 'Dec',
+        fromYear: '2017',
+        toDay: 15,
+        toMonth: 'Jan',
+        toYear: '2018'
+      }
+    ]
+  }) ;
 }
