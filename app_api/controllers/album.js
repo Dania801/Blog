0beb71f31f45d17c2b3ac6904d07f5343191adf1
Dania801 <ssansnnom07@gmail.com>
2018-01-15 20:11:55 +0200

    REST api pushing new picture to DB

--- app_api/controllers/album.js
+++ app_api/controllers/album.js
@@ -23,10 +23,45 @@ module.exports.getAlbum = function(req, res){
 };
 
 module.exports.getPicture = function(req, res){
-
+  if(req.params && req.params.userid && req.params.picid){
+    Blog
+      .find({"_id": req.params.userid}, (err, user) =>{
+        let album = user[0].album.filter(pic => pic._id == req.params.picid);
+        if(err){
+          sendJsonResponse(res, 404, err);
+        }else if(!album){
+          sendJsonResponse(res, 404, {"message": "No events found"});
+        }else{
+          sendJsonResponse(res, 200, album[0]);
+        }
+      });
+  }else{
+      if(!req.params.userid){
+        sendJsonResponse(res, 404, {"message": "userid isn't givin!"});
+      }else if(!req.params.picid){
+        sendJsonResponse(res, 404, {"message": "picid isn't givin!"});
+      }else{
+        sendJsonResponse(res, 404, {"message": "Missed parameters!"});
+      }
+  }
 };
 
 module.exports.createPicture = function(req, res){
+  if(req.params && req.params.userid){
+    Blog
+      .update({"_id": req.params.userid}, {$push: {'album' : {
+        image: req.body.image,
+        caption: req.body.caption
+      }}},(err, album)=> {
+        if(err){
+          sendJsonResponse(res, 404, err);
+        }else{
+          sendJsonResponse(res, 201, album);
+        }
+      })
+  }else{
+    sendJsonResponse(res, 404, {"message": "userid isn't givin!"});
+  }
 
 };
 
--- app_api/routes/index.js
+++ app_api/routes/index.js
@@ -14,19 +14,19 @@ router.delete('/user/:userid', ctrlUsers.deleteUser);
 
 router.get('/user/:userid/articles', ctrlArticles.getArticlesList);
 //router.get('/user/:userid/articles/:articleid', ctrlArticles.getArticle);
-router.post('/user/:userid/articles', ctrlArticles.createArticle);
+router.post('/user/:userid/articles', ctrlArticles.createArticle); //Done
 router.put('/user/:userid/articles/:articleid', ctrlArticles.updateArticle);
 router.delete('/user/:userid/articles/:articleid', ctrlArticles.deleteArticle);
 
-router.get('/user/:userid/album', ctrlAlbum.getAlbum);
-router.get('/user/:userid/album/:pic', ctrlAlbum.getPicture);
+router.get('/user/:userid/album', ctrlAlbum.getAlbum); //Done
+router.get('/user/:userid/album/:picid', ctrlAlbum.getPicture); //Done
 router.post('/user/:userid/album', ctrlAlbum.createPicture);
-router.put('/user/:userid/album/:pic', ctrlAlbum.updatePicture);
-router.delete('/user/:userid/album/:pic', ctrlAlbum.deletePicture);
+router.put('/user/:userid/album/:picid', ctrlAlbum.updatePicture);
+router.delete('/user/:userid/album/:picid', ctrlAlbum.deletePicture);
 
 router.get('/user/:userid/events', ctrlEvents.getEventsList); //Done
 router.get('/user/:userid/events/:eventid', ctrlEvents.getEvent); //Done
-router.post('/user/:userid/events', ctrlEvents.createEvent);
+router.post('/user/:userid/events', ctrlEvents.createEvent);//Done // Still some work is needed in formating date , and default endDate
 router.put('/user/:userid/events/:eventid', ctrlEvents.updateEvent);
 router.delete('/user/:userid/event/:eventid', ctrlEvents.deleteEvent);
 
